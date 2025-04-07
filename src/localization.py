73ad6a25f3664138f3e42bd455f780aa4af5fa7e
Alejo Flores Lucey <alejo@misterflores.com>
2025-04-07 12:24:12 +0000

    feat: Predict robot position using kNN

--- src/localization.py
+++ src/localization.py
@@ -1,5 +1,7 @@
 import numpy as np
 import rospy
+from typing import Tuple
+from sklearn.neighbors import KNeighborsClassifier
 from sensor_msgs.msg import LaserScan
 from nav_msgs.srv import GetMap
 from nav_msgs.msg import OccupancyGrid
@@ -27,14 +29,14 @@ def deserialize_map(data: OccupancyGrid) -> Map:
     Returns:
         Map: deserialized map object
     """
-    coordinates = lambda i, j: (i * data.info.resolution + data.info.origin.position.x + data.info.resolution / 2,
-                                j * data.info.resolution + data.info.origin.position.y + data.info.resolution / 2)
+    coordinates = lambda i, j: (i * data.info.resolution + data.info.origin.position.x + (data.info.resolution / 2),
+                                j * data.info.resolution + data.info.origin.position.y + (data.info.resolution / 2))
     
     walls = np.reshape(data.data, (data.info.height, data.info.width))
     map = np.zeros((data.info.height, data.info.width), dtype=Point)
     for y in range(data.info.height):
         for x in range(data.info.width):
-            p = MapPoint(*coordinates(x, data.info.height - y), walls[x][y] == 100)
+            p = MapPoint(*coordinates(x, y), walls[y][x] == 100)
             map[y][x] = p
 
     return Map(
@@ -80,3 +82,49 @@ def get_laser_scan(map_resolution: float) -> np.ndarray:
         points.append(Point(x, y))
 
     return np.array(points)
+
+
+def predict_robot_position(map: Map, laser_scan: np.ndarray) -> Tuple[float, float]:
+    """
+    Predict the robot position based on the laser scan data, using K-Nearest Neighbors (kNN) algorithm.
+    
+    Args:
+        map (Map): map object containing the map data
+        laser_scan (np.ndarray): laser scan data
+        
+    Returns:
+        Tuple[float, float]: predicted position of the robot (x, y)
+    """
+    real_amount_of_walls = len(laser_scan)
+    x_train = []
+    y_train = []
+    for point in map.map.flatten():
+        x_train.append((point.x, point.y))
+        y_train.append(1 if point.wall else 0)
+
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(x_train, y_train)
+
+    possible_positions = []
+    for i in np.arange(0, np.round(map.width * map.resolution), 1):
+        for j in np.arange(0, map.height * map.resolution, map.resolution):
+            possible_positions.append((i, j))
+
+    possible_laser_scans = []
+    for x, y in possible_positions:
+        new_laser_scan = np.array([(p.x + x, p.y + y) for p in laser_scan])
+        possible_laser_scans.append(new_laser_scan)
+
+    amount_of_walls = []
+    for laser_scan in possible_laser_scans:
+        amount_of_walls.append(np.sum(knn.predict(laser_scan)))
+
+    position_candidate = None
+    min_distance = np.inf
+    for position, amount in zip(possible_positions, amount_of_walls):
+        distance = abs(amount - real_amount_of_walls)
+        if distance < min_distance:
+            position_candidate = position
+            min_distance = distance
+
+    return position_candidate
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import rospy
-from localization import get_map, deserialize_map, get_laser_scan
+from localization import get_map, deserialize_map, get_laser_scan, predict_robot_position
 from visualization import plot_map_and_laser_scan
 
 
@@ -9,4 +9,6 @@ if __name__ == "__main__":
     map_data = get_map()
     map = deserialize_map(map_data)
     laser_scan = get_laser_scan(map.resolution)
-    plot_map_and_laser_scan(map, laser_scan)
+    # plot_map_and_laser_scan(map, laser_scan)
+    robot_position = predict_robot_position(map, laser_scan)
+    print(f"Robot position: {robot_position}")

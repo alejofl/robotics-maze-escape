37ce9b204361ad04f8f7890bb18248f37b1ae73b
Alejo Flores Lucey <alejo@misterflores.com>
2025-04-08 11:40:45 +0000

    feat: Implement factory pattern for plotter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import rospy
+import numpy as np
 from custom_types import Point
 from localization import get_map, deserialize_map, get_laser_scan, predict_robot_position
 from global_planning import GlobalPlanner
-from visualization import plot_map_with_initial_position, plot_map_with_global_plan
+from visualization import Plotter
 
 
 if __name__ == "__main__":
@@ -11,7 +12,25 @@ if __name__ == "__main__":
     map_data = get_map()
     map = deserialize_map(map_data)
     laser_scan = get_laser_scan(map.resolution)
-    # plot_map_with_initial_position(map, laser_scan, Point(0, 0))
+    Plotter() \
+        .with_size(8, 8) \
+        .with_map(map) \
+        .with_laser_scan(laser_scan) \
+        .with_robot(Point(0, 0)) \
+        .with_title("Map and Laser Scan Data Transformed into World Coordinates") \
+        .with_labels("x [m]", "y [m]") \
+        .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
+        .with_grid() \
+        .show()
     robot_position_idx, robot_position, laser_scan  = predict_robot_position(map, laser_scan)
     global_path = GlobalPlanner.get_planner("bfs", map, robot_position_idx).plan()
-    # plot_map_with_global_plan(map, laser_scan, robot_position, global_path)
+    Plotter() \
+        .with_size(8, 8) \
+        .with_map(map) \
+        .with_robot(robot_position) \
+        .with_global_plan(global_path) \
+        .with_title("Found Path from Robot Position to Goal Position") \
+        .with_labels("x [m]", "y [m]") \
+        .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
+        .with_grid() \
+        .show()
--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,4 @@
 import matplotlib.pyplot as plt
-from matplotlib.axes import Axes
-from typing import Tuple
 import numpy as np
 from custom_types import Map, Point
 
@@ -14,117 +12,131 @@ COLORS = {
 }
 
 
-def plot_map(ax: Axes, map: Map):
-    """
-    Plot the map in world coordinates.
+class Plotter:
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
 
-    Args:
-        ax (Axes): matplotlib axes object
-        map (Map): map object containing the map data
-    """
-    walls_xs = []
-    walls_ys = []
-    points_xs = []
-    points_ys = []
-    for point in map.map.flatten():
-        if point.wall:
-            walls_xs.append(point.x)
-            walls_ys.append(point.y)
-        else:
-            points_xs.append(point.x)
-            points_ys.append(point.y)
-    ax.scatter(walls_xs, walls_ys, c=COLORS["darkblue"], alpha=1.0, s=6**2, label="Walls")
-    ax.scatter(points_xs, points_ys, c=COLORS["twgrey"], alpha=0.08, s=6**2, label="Unobstructed Space")
+    def with_size(self, width: float, height: float):
+        """
+        Set the figure size.
 
+        Args:
+            width (float): width of the figure, in inches
+            height (float): height of the figure, in inches
+        """
+        self.fig.set_size_inches(width, height)
+        return self
 
-def plot_laser_scan(ax: Axes, laser_scan: np.ndarray):
-    laser_scan_xs = []
-    laser_scan_ys = []
-    for point in laser_scan:
-        laser_scan_xs.append(point.x)
-        laser_scan_ys.append(point.y)
-    ax.scatter(laser_scan_xs, laser_scan_ys, c="r", label="Laser Scan")
+    def with_map(self, map: Map):
+        """
+        Plot the map in world coordinates.
 
+        Args:
+            map (Map): map object containing the map data
+        """
+        walls_xs = []
+        walls_ys = []
+        points_xs = []
+        points_ys = []
+        for point in map.map.flatten():
+            if point.wall:
+                walls_xs.append(point.x)
+                walls_ys.append(point.y)
+            else:
+                points_xs.append(point.x)
+                points_ys.append(point.y)
+        self.ax.scatter(walls_xs, walls_ys, c=COLORS["darkblue"], alpha=1.0, s=6**2, label="Walls")
+        self.ax.scatter(points_xs, points_ys, c=COLORS["twgrey"], alpha=0.08, s=6**2, label="Unobstructed Space")
+        return self
 
-def plot_robot_position(ax: Axes, robot_position: Point):
-    """
-    Plot the robot position in world coordinates.
+    def with_laser_scan(self, laser_scan: np.ndarray):
+        """
+        Plot the laser scan data in world coordinates.
 
-    Args:
-        ax (Axes): matplotlib axes object
-        robot_position (Point): robot position in world coordinates
-    """
-    ax.scatter([robot_position.x], [robot_position.y], c=COLORS["twblue"], s=15**2, label="Scan Center")
+        Args:
+            laser_scan (np.ndarray): laser scan data
+        """
+        laser_scan_xs = []
+        laser_scan_ys = []
+        for point in laser_scan:
+            laser_scan_xs.append(point.x)
+            laser_scan_ys.append(point.y)
+        self.ax.scatter(laser_scan_xs, laser_scan_ys, c="r", label="Laser Scan")
+        return self
 
+    def with_robot(self, robot_position: Point):
+        """
+        Plot the robot position in world coordinates.
 
-def plot_global_plan(ax: Axes, global_path: np.ndarray):
-    """
-    Plot the global path in world coordinates.
+        Args:
+            robot_position (Point): robot position in world coordinates
+        """
+        self.ax.scatter([robot_position.x], [robot_position.y], c=COLORS["twblue"], s=15**2, label="Scan Center")
+        return self
+        
+    def with_global_plan(self, global_path: np.ndarray):
+        """
+        Plot the global path in world coordinates.
 
-    Args:
-        ax (Axes): matplotlib axes object
-        global_path (np.ndarray): global path as an array of points
-    """
-    path_xs = []
-    path_ys = []
-    for point in global_path:
-        path_xs.append(point.x)
-        path_ys.append(point.y)
-    ax.plot(path_xs, path_ys, "-o", c=COLORS["lightblue"], label="Global Path")
+        Args:
+            global_path (np.ndarray): global path as an array of points
+        """
+        path_xs = []
+        path_ys = []
+        for point in global_path:
+            path_xs.append(point.x)
+            path_ys.append(point.y)
+        self.ax.plot(path_xs, path_ys, "-o", c=COLORS["lightblue"], label="Global Path")
+        return self
 
+    def with_title(self, title: str):
+        """
+        Set the title of the plot.
 
-def plot_map_with_initial_position(map: Map, laser_scan: np.ndarray, robot_position: Point):
-    """
-    Plot the map and laser scan data in world coordinates.
+        Args:
+            title (str): title of the plot
+        """
+        self.ax.set_title(title)
+        return self
+        
+    def with_labels(self, xlabel: str, ylabel: str):
+        """
+        Set the labels of the plot.
 
-    Args:
-        map (Map): map object containing the map data
-        laser_scan (np.ndarray): laser scan data
-        robot_position (Point): robot position in world coordinates
-    """
-    fig, ax = plt.subplots()
-    fig.set_size_inches(8, 8)
-    
-    plot_map(ax, map)
-    plot_laser_scan(ax, laser_scan)
-    plot_robot_position(ax, robot_position)
+        Args:
+            xlabel (str): x-axis label
+            ylabel (str): y-axis label
+        """
+        if xlabel is not None:
+            self.ax.set_xlabel(xlabel)
+        if ylabel is not None:
+            self.ax.set_ylabel(ylabel)
+        return self
 
-    ax.set_xlabel("x [m]")
-    ax.set_ylabel("y [m]")
-    ax.set_title("Map and Laser Scan Data Transformed into World Coordinates")
-    ax.set_xticks(np.arange(-1, 5, 1))
-    ax.set_yticks(np.arange(-1, 5, 1))
-    ax.set_axisbelow(True)
-    ax.grid()
-    ax.legend()
+    def with_ticks(self, xticks: np.ndarray, yticks: np.ndarray):
+        """
+        Set the ticks of the plot.
 
-    plt.show()
+        Args:
+            xticks (np.ndarray): x-axis ticks
+            yticks (np.ndarray): y-axis ticks
+        """
+        if xticks is not None:
+            self.ax.set_xticks(xticks)
+        if yticks is not None:
+            self.ax.set_yticks(yticks)
+        return self
 
+    def with_grid(self):
+        """
+        Set the grid of the plot.
+        """
+        self.ax.grid()
+        return self
 
-def plot_map_with_global_plan(map: Map, laser_scan: np.ndarray, robot_position: Point, global_path: np.ndarray):
-    """
-    Plot the map and laser scan data in world coordinates.
-
-    Args:
-        map (Map): map object containing the map data
-        laser_scan (np.ndarray): laser scan data
-        robot_position (Tuple[float, float]): robot position in world coordinates
-    """
-    fig, ax = plt.subplots()
-    fig.set_size_inches(8, 8)
-    
-    plot_map(ax, map)
-    plot_laser_scan(ax, laser_scan)
-    plot_robot_position(ax, robot_position)
-    plot_global_plan(ax, global_path)
-
-    ax.set_xlabel("x [m]")
-    ax.set_ylabel("y [m]")
-    ax.set_title("Found Path from Robot Position to Goal Position")
-    ax.set_xticks(np.arange(-1, 5, 1))
-    ax.set_yticks(np.arange(-1, 5, 1))
-    ax.set_axisbelow(True)
-    ax.grid()
-    ax.legend()
-
-    plt.show()
+    def show(self):
+        """
+        Show the plot.
+        """
+        self.ax.legend()
+        plt.show()

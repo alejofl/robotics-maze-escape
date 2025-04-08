27c106cf77e87869c550c62c9153bbb4e96f0880
Alejo Flores Lucey <alejo@misterflores.com>
2025-04-08 13:07:16 +0000

    feat: Add publisher for global path

---
+++ src/emission.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+import rospy
+import uuid
+from typing import Any, List
+from custom_types import Point
+from nav_msgs.msg import Path as RosPath
+from geometry_msgs.msg import PoseStamped as RosPoseStamped
+
+
+class Emitter(ABC):
+    def __init__(self, topic: str, message_type: Any):
+        self.publisher = rospy.Publisher(topic, message_type, queue_size=10)
+
+    @abstractmethod
+    def emit(self, data: Any):
+        """
+        Emit data.
+        
+        Args:
+            data (Any): The data to emit.
+        """
+        pass
+
+
+class GlobalPlanEmmiter(Emitter):
+    def __init__(self):
+        super().__init__("/myPath", RosPath)
+
+    def emit(self, data: Any):
+        data: List[Point] = data
+        message = RosPath()
+
+        message.header.seq = 0
+        message.header.stamp = rospy.Time.now()
+        message.header.frame_id = "odom"
+        for point in data:
+            pose = RosPoseStamped()
+            pose.header.seq = 0
+            pose.header.stamp = rospy.Time.now()
+            pose.header.frame_id = "odom"
+            pose.pose.position.x = point.x
+            pose.pose.position.y = point.y
+            pose.pose.position.z = 0.0
+            message.poses.append(pose)
+
+        self.publisher.publish(message)
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from custom_types import Point
 from localization import get_map, deserialize_map, get_laser_scan, predict_robot_position
 from global_planning import GlobalPlanner
 from visualization import Plotter
+from emission import GlobalPlanEmmiter
 
 
 if __name__ == "__main__":
@@ -12,25 +13,26 @@ if __name__ == "__main__":
     map_data = get_map()
     map = deserialize_map(map_data)
     laser_scan = get_laser_scan(map.resolution)
-    Plotter() \
-        .with_size(8, 8) \
-        .with_map(map) \
-        .with_laser_scan(laser_scan) \
-        .with_robot(Point(0, 0)) \
-        .with_title("Map and Laser Scan Data Transformed into World Coordinates") \
-        .with_labels("x [m]", "y [m]") \
-        .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
-        .with_grid() \
-        .show()
+    # Plotter() \
+    #     .with_size(8, 8) \
+    #     .with_map(map) \
+    #     .with_laser_scan(laser_scan) \
+    #     .with_robot(Point(0, 0)) \
+    #     .with_title("Map and Laser Scan Data Transformed into World Coordinates") \
+    #     .with_labels("x [m]", "y [m]") \
+    #     .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
+    #     .with_grid() \
+    #     .show()
     robot_position_idx, robot_position, laser_scan  = predict_robot_position(map, laser_scan)
     global_path = GlobalPlanner.get_planner("bfs", map, robot_position_idx).plan()
-    Plotter() \
-        .with_size(8, 8) \
-        .with_map(map) \
-        .with_robot(robot_position) \
-        .with_global_plan(global_path) \
-        .with_title("Found Path from Robot Position to Goal Position") \
-        .with_labels("x [m]", "y [m]") \
-        .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
-        .with_grid() \
-        .show()
+    # Plotter() \
+    #     .with_size(8, 8) \
+    #     .with_map(map) \
+    #     .with_robot(robot_position) \
+    #     .with_global_plan(global_path) \
+    #     .with_title("Found Path from Robot Position to Goal Position") \
+    #     .with_labels("x [m]", "y [m]") \
+    #     .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
+    #     .with_grid() \
+    #     .show()
+    GlobalPlanEmmiter().emit(global_path)

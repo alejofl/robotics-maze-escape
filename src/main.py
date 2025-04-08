f033b34318bbad76156645c1035115528ba8138e
Alejo Flores Lucey <alejo@misterflores.com>
2025-04-08 13:19:12 +0000

    feat: Add parameters to launchfile to conditionally plot and select global planning algorithm

--- src/main.py
+++ src/main.py
@@ -9,30 +9,35 @@ from emission import GlobalPlanEmmiter
 
 
 if __name__ == "__main__":
-    rospy.init_node("maze-escape")
+    rospy.init_node("maze_escape")
+    global_planner_algorithm = rospy.get_param("/maze_escape/global_planner_algorithm")
+    enable_plotting = rospy.get_param('/maze_escape/enable_plotting')
+
     map_data = get_map()
     map = deserialize_map(map_data)
     laser_scan = get_laser_scan(map.resolution)
-    # Plotter() \
-    #     .with_size(8, 8) \
-    #     .with_map(map) \
-    #     .with_laser_scan(laser_scan) \
-    #     .with_robot(Point(0, 0)) \
-    #     .with_title("Map and Laser Scan Data Transformed into World Coordinates") \
-    #     .with_labels("x [m]", "y [m]") \
-    #     .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
-    #     .with_grid() \
-    #     .show()
+    if enable_plotting:
+        Plotter() \
+            .with_size(8, 8) \
+            .with_map(map) \
+            .with_laser_scan(laser_scan) \
+            .with_robot(Point(0, 0)) \
+            .with_title("Map and Laser Scan Data Transformed into World Coordinates") \
+            .with_labels("x [m]", "y [m]") \
+            .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
+            .with_grid() \
+            .show()
     robot_position_idx, robot_position, laser_scan  = predict_robot_position(map, laser_scan)
-    global_path = GlobalPlanner.get_planner("bfs", map, robot_position_idx).plan()
-    # Plotter() \
-    #     .with_size(8, 8) \
-    #     .with_map(map) \
-    #     .with_robot(robot_position) \
-    #     .with_global_plan(global_path) \
-    #     .with_title("Found Path from Robot Position to Goal Position") \
-    #     .with_labels("x [m]", "y [m]") \
-    #     .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
-    #     .with_grid() \
-    #     .show()
+    global_path = GlobalPlanner.get_planner(global_planner_algorithm, map, robot_position_idx).plan()
+    if enable_plotting:
+        Plotter() \
+            .with_size(8, 8) \
+            .with_map(map) \
+            .with_robot(robot_position) \
+            .with_global_plan(global_path) \
+            .with_title("Found Path from Robot Position to Goal Position") \
+            .with_labels("x [m]", "y [m]") \
+            .with_ticks(np.arange(-1, 5, 1), np.arange(-1, 5, 1)) \
+            .with_grid() \
+            .show()
     GlobalPlanEmmiter().emit(global_path)
